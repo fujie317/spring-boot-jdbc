cd14c750e669af7bb50d4c3cc0848b804f535f01
Fujie Zhang <fzhang556@gmail.com>
2019-03-12 15:33:35 +0800

    remove data.sql and schema.sql which only with embeded databases

--- spring-boot-jdbc/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-insert into stock values(1, 'VMWare', 'VMW', 58.56);
-insert into stock values(2, 'AT&T', 'T', 38.74);
-insert into stock values(3, 'Facebook', 'FB', 118.25);
--- spring-boot-jdbc/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-drop table if exists stock;
-
-create table stock(
-	stock_id int,
-	company_name varchar(50),
-	symbol varchar(10),
-	price decimal(10,2)
-);
